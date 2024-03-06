4a5991f2637fd0cb8cdc642c3fde612841e9cf7d
Beatriiz_Cavalcante <biamenezes193@gmail.com>
2024-03-06 15:47:04 -0300

    Variáveis e Conversão

---
+++ aula1.py
@@ -0,0 +1,26 @@
+print("hello world!")
+
+
+#Conversão de tipos
+
+#Float para inteiro 
+preco = 10.30
+preco = int(preco)
+print(preco)
+
+#Numérico para string
+
+data = 2001
+idade = 22
+
+print(str(data))
+print(str(preco))
+
+print(f"Nasceu em {data}, logo, tem {idade} anos") #exemplo de concatenação str e int
+
+
+#String para número 
+valor="10.50"
+
+print(float(valor))
+print(int())
--- aulao.py
+++
@@ -1,8 +0,0 @@
-print("hello world!")
-
-preco = 10.30
-idade = 90
-
-print(type(preco))
-preco = str(preco)
-print(type(preco))
